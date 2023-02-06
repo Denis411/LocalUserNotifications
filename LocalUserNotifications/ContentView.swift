61f6a802ff3e7c2d2b485d1f5fe7296f611f8631
Dennis <dnsljnv411@gmail.com>
2023-02-06 12:57:40 +0700

    Initial Commit

---
+++ LocalUserNotifications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalUserNotifications
+//
+//  Created by Dennis Programmer on 6/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalUserNotifications/LocalUserNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalUserNotificationsApp.swift
+//  LocalUserNotifications
+//
+//  Created by Dennis Programmer on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalUserNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
